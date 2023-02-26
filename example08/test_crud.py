5c8073f0e8cbbab6812cd816dc4b2b5e36ac68db
Flávio Micheletti <flaviomicheletti@hotmail.com>
2023-02-26 13:26:47 -0300

    example08

--- example08/test_crud.py
+++ example08/test_crud.py
@@ -3,48 +3,36 @@ from unittest.mock import patch, Mock
 
 from example08.crud import create, read, update, delete
 
-
-
 class TestCrudOperations(unittest.TestCase):
     def setUp(self):
         self.mock_conn = Mock()
+        self.mock_cursor = self.mock_conn.cursor.return_value
 
     def test_create(self):
-        # mock the cursor object
-        mock_cursor = self.mock_conn.cursor.return_value
         # call the create function
         create(self.mock_conn, "Alice", 25)
         # assert that the expected SQL query was executed
-        # mock_cursor.execute.assert_called_once_with("INSERT INTO example_table (name, age) VALUES (?, ?)", ("Alice", 25))
+        self.mock_conn.execute.assert_called_once_with("INSERT INTO example_table (name, age) VALUES (?, ?)", ("Alice", 25))
         self.mock_conn.commit.assert_called_once()
 
-
     def test_read(self):
-        # mock the cursor object
-        mock_cursor = self.mock_conn.cursor.return_value
         # set up some example data
         example_data = [(1, "Alice", 25), (2, "Bob", 30), (3, "Charlie", 35)]
-        mock_cursor.fetchall.return_value = example_data
+        self.mock_cursor.fetchall.return_value = example_data
         # call the read function
         records = read(self.mock_conn)
-        # assert that the expected SQL query was executed and the correct data was returned
-        # mock_cursor.execute.assert_called_once_with("SELECT * FROM example_table")
-        # self.assertEqual(records, example_data)
         self.assertEqual(records, [])
+        self.mock_conn.execute.assert_called_once_with("SELECT * FROM example_table")
 
     def test_update(self):
-        # mock the cursor object
-        mock_cursor = self.mock_conn.cursor.return_value
         # call the update function
         update(self.mock_conn, 1, "Alice", 30)
-        # assert that the expected SQL query was executed
-        # mock_cursor.execute.assert_called_once_with("UPDATE example_table SET name=?, age=? WHERE id=?", ("Alice", 30, 1))
+        self.mock_conn.execute.assert_called_once_with("UPDATE example_table SET name=?, age=? WHERE id=?", ("Alice", 30, 1))
         self.mock_conn.commit.assert_called_once()
 
     def test_delete(self):
-        # mock the cursor object
-        mock_cursor = self.mock_conn.cursor.return_value
         # call the delete function
         delete(self.mock_conn, 1)
         # assert that the expected SQL query was executed
-        mock_cursor.execute.assert_called_once
+        self.mock_conn.execute.assert_called_once_with("DELETE FROM example_table WHERE id=?", (1,))
+        self.mock_conn.commit.assert_called_once()
